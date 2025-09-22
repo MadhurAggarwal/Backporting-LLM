4d05e4b3075bebc7b1b6f86bacd2f80408568856
Ubuntu <sumsharma@aiVm.vpdkcxbu153ufc3s2lcxuzxnlf.dx.internal.cloudapp.net>
2025-09-22 08:29:43 +0000

    Added results for test runs

--- finetuning/azureLLM_handler.py
+++ finetuning/azureLLM_handler.py
@@ -1,5 +1,5 @@
 import os
-from openai import OpenAI
+from openai import OpenAI, AzureOpenAI
 from dotenv import load_dotenv
 import json
 
@@ -10,6 +10,8 @@ class AzureLLMHandler:
         self.api_key          = os.getenv("AZURE_OPENAI_API_KEY")
         self.api_endpoint     = os.getenv("AZURE_OPENAI_ENDPOINT")
         self.azure_deployment = os.getenv("AZURE_OPENAI_DEPLOYMENT")
+        self.finetuned_deployment = os.getenv("AZURE_OPENAI_FINETUNED_DEPLOYEMENT")
+        self.finetuned_endpoint = os.getenv("AZURE_OPENAI_FINETUNED_ENDPOINT")
 
         if not self.api_endpoint:
             raise ValueError(
@@ -31,6 +33,12 @@ class AzureLLMHandler:
             base_url=self.api_endpoint,
             )
 
+        self.finetuned_client = AzureOpenAI(
+            api_version="2024-12-01-preview",
+            api_key=self.api_key,
+            base_url=self.finetuned_endpoint,
+        )
+
     def call_azure_openai(self, system_prompt, user_prompt, temperature=0.7, max_tokens=4000, top_p=0.9):
         try:
             response = self.client.chat.completions.create(
@@ -48,6 +56,25 @@ class AzureLLMHandler:
             
         except Exception as e:
             print(f"Error calling Azure OpenAI: {e}")
+            return ""
+
+    def call_azure_openai_finetuned(self, system_prompt, user_prompt, temperature=0.7, max_tokens=4000, top_p=0.9):
+        try:
+            response = self.finetuned_client.chat.completions.create(
+                model=self.finetuned_deployment,
+                messages=[
+                    {"role": "system", "content": system_prompt},
+                    {"role": "user", "content": user_prompt}
+                ],
+                temperature=temperature,
+                max_tokens=max_tokens,
+                top_p=top_p
+            )
+
+            return response.choices[0].message.content.strip()
+            
+        except Exception as e:
+            print(f"Error calling Azure OpenAI Finetuned: {e}")
             return ""
 
     def call_azure_openai_for_qna_schema(self, system_prompt, user_prompt, temperature=0.7, max_tokens=4000, top_p=0.9):
@@ -95,16 +122,68 @@ class AzureLLMHandler:
         return output
 
 def main():
-    system_prompt = """
-    You are an expert software developer. Answer the questions about DSA.
-    DO NOT Give explanations, only code.
-    """
+    # system_prompt = """
+    # You are an expert software developer. Answer the questions about DSA.
+    # DO NOT Give explanations, only code.
+    # """
 
-    user_prompt = "question-1: Give code for quick sort in python. question-2: give code for binary search in python"
+    # user_prompt = "question-1: Give code for quick sort in python. question-2: give code for binary search in python"
 
-    azure_handler = AzureLLMHandler()
-    response = azure_handler.call_azure_openai_for_qna_schema(system_prompt, user_prompt)
-    print(response)
+    # system_prompt = """
+    # You are an expert software developer. Answer the questions about DSA.
+    # DO NOT Give explanations, only code.
+    # """
+
+    # user_prompt = "question-1: Give code for quick sort in python. question-2: give code for binary search in python"
+
+    # azure_handler = AzureLLMHandler()
+    # response = azure_handler.call_azure_openai_finetuned(system_prompt, user_prompt)
+    # print(response)
+
+    endpoint = "https://aipkgmgmnt-resource.cognitiveservices.azure.com/"
+    model_name = "gpt-4o"
+    deployment = "gpt-4o-2024-08-06-commit-history-libsoup-1"
+
+    load_dotenv()
+    subscription_key = os.getenv("AZURE_OPENAI_API_KEY")
+    api_version = "2024-12-01-preview"
+
+    client = AzureOpenAI(
+        api_version=api_version,
+        azure_endpoint=endpoint,
+        api_key=subscription_key,
+    )
+
+    response = client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful assistant, with understanding of libsoup commit history. Based on commit history, find the likely location of backporting",
+            },
+            {
+                "role": "user",
+                "content": """
+                In Latest Version of Libsoup, the function read_internal is defined in the file 'soup-message-io-data.c'.
+                Was this function moved? if yes, from which file to which file? 
+"""
+#                 "content": """I have a patch on the LATEST liboup repo that contains changes to 
+#                 --- a/libsoup/http-1/sb-output-stream.c
+# +++ b/libsoup/http-1/sb-output-stream.c
+# @@ -349,7 +349,11 @@ soup_body_output_stream_create_source (GPollableOutputStream *stream,
+ 
+
+# Based on your knowledge of git commit history, & how filenames, function names & contents have changed with commits,
+# what is the likely location (filename, functionname) of the backporting commit in the OLD libsoup git repository?
+#                 """,
+            }
+        ],
+        max_tokens=4096,
+        temperature=0.5,
+        top_p=1.0,
+        model=deployment
+    )
+
+    print(response.choices[0].message.content)
 
 if __name__ == "__main__":
     main()
--- libsoup_custom_changes_for_swe_test/test_patches/test-readme.md
+++ libsoup_custom_changes_for_swe_test/test_patches/test-readme.md
@@ -23,10 +23,23 @@ git add .
 git commit -m "Test-patch-1"
 ```
 
+Get the Latest Commit ID. This is your `cid`, that the SWE-Agent will try to backport onto some older version.
+```
+git show HEAD
+```
+
+On the libsoup folder, run:
+```
+git checkout 3.4.4
+```
+
+This is the old version to which SWE-Agent will backport the cid patch to.
+
 Now, Run [SWE-AGENT](https://dev.azure.com/mariner-org/mariner/_git/AllSpark) with either the Base or Finetuned Model  
 For running with the Finetuned Model, use 
 - export LITELLM_LOCAL_MODEL_COST_MAP=True
-- Add details about finetuned model (name, tokens, etc) in the model_prices_and_context_window_backup.json file
+- Add details about finetuned model (name, tokens, etc) in the .venv/lib/python3.13/site-packages/litellm/model_prices_and_context_window_backup.json file
+- Add the .env in SWE Agent to include AZURE_API_KEY and AZURE_API_BASE
 
 Then use SWE Agent to Backport latest local HEAD to tag: 3.4.4
 
@@ -34,3 +47,33 @@ Test-1 tests how well can the Base/Finetuned Models in the SWE Agent backport ch
 
 ---
 Results
+
+## Test-2: Filepath & Function-name Changes
+
+Prepare Libsoup as before
+``` 
+git checkout master
+git reset --hard b3df7199959e8661d6cf9d65ffbfb94692982b3a
+
+git am /home/sumsharma/madhur/backporting-llm/training_llm/libsoup_custom_changes_for_swe_test/patches/*.patch
+```
+
+Now Apply the second test patch:
+```
+patch -p1 < /home/sumsharma/madhur/backporting-llm/training_llm/libsoup_custom_changes_for_swe_test/test_patches/test-2-function-location-and-filepath-change.patch
+
+git add .
+git commit -m "Test-patch-2"
+```
+
+## Test 3
+
+```
+git checkout master
+git reset --hard b3df7199959e8661d6cf9d65ffbfb94692982b3a
+
+git am /home/sumsharma/madhur/backporting-llm/training_llm/libsoup_custom_changes_for_swe_test/patches/0001-Renamed-Files-Moved-file-paths.patch
+git am /home/sumsharma/madhur/backporting-llm/training_llm/libsoup_custom_changes_for_swe_test/patches/0002-Added-Random-Lines-to-sb-output-stream.c.patch
+git am /home/sumsharma/madhur/backporting-llm/training_llm/libsoup_custom_changes_for_swe_test/patches/0003-Added-More-Random-Lines-to-sb-output-stream.c.patch
+git am /home/sumsharma/madhur/backporting-llm/training_llm/libsoup_custom_changes_for_swe_test/patches/0004-Refactored-function-into-multiple-different-function.patch
+```
